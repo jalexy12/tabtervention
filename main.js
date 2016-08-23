15c19ecf405f1acd06ac613f5ec615281a3fefec
Josh <jalexy12@gmail.com>
2016-08-23 00:11:19 -0400

    Updating times on a chrome alarm interval

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-console.log("DOM Content loaded")
-
-// When to load this efficiently?
+// MS
+var TIME_LIMIT = 864000000
+var OLD_THRESHHOLD = 259200000
+var FINE_THRESHHOLD = 86400000
 
 function setupTabStorage(){
 	chrome.storage.local.get("tabs", function(res){
@@ -16,7 +17,8 @@ function setupTabStorage(){
 					var diff = current - openedOn
 
 					storageObj[tab.id] = {
-						opened: tab.opened,
+						title: tab.title,
+						opened: foundTab.opened,
 						openFor: diff
 					}
 
@@ -24,7 +26,8 @@ function setupTabStorage(){
 
 				} else {
 					console.log("Doesnt have tab", tab)
-					storageObj[tab.id] = { 
+					storageObj[tab.id] = {
+						title: tab.title, 
 						opened: (new Date()).getTime(), 
 						openFor: 0
 					}
@@ -38,8 +41,81 @@ function setupTabStorage(){
 }
 
 function checkIfOld(){
-	
-}
-setupTabStorage()
 
+	chrome.storage.local.get("tabs", function(res){
+		var newObj = {}
+
+		for (var key in res.tabs) {
+			if (Number(res.tabs[key].openFor) < TIME_LIMIT){
+				newObj[key] = res.tabs[key]
+				var opened = newObj[key].opened
+				var current = (new Date()).getTime()
+				newObj[key].openFor = current - opened				
+			} else {
+				console.log("Time To Close...", res.tabs[key])
+			}
+		}
+
+		chrome.storage.local.set({tabs: newObj}, function(){
+			console.log("Updated!")
+		})
+		console.log(newObj)
+		updateDomWithTabs(newObj)
+	})
+
+}
+
+function updateDomWithTabs(tabs){
+	var classified = {
+		aboutToExpire: [],
+		older: [],
+		fine: []
+	}
+
+	for (var key in tabs){
+		var currentTab = tabs[key]
+
+		if (currentTab.openFor <= FINE_THRESHHOLD){
+			classified.fine.push(currentTab)
+		} else if(currentTab.openFor > FINE_THRESHHOLD && currentTab.openFor <= OLD_THRESHHOLD){
+			classified.older.push(currentTab)
+		} else {
+			classified.aboutToExpire.push(currentTab)
+		}
+	}
+
+
+	for (var classification in classified){
+		classified[classification].forEach(function(tab){
+			var appendTo = document.getElementById(classification)
+			var newLi = document.createElement("li")
+			newLi.appendChild(document.createTextNode(tab.title))
+			appendTo.appendChild(newLi)
+		})
+	}
+}
+
+function cleanUp(){
+	chrome.storage.local.clear()
+	chrome.alarms.clearAll()
+}
+
+function init(){
+	setupTabStorage()
+	chrome.alarms.create("tabCheck", {when: Date.now() + 60000, periodInMinutes: 1})
+}
+
+chrome.alarms.onAlarm.addListener(function(alarm){
+	if (alarm.name === "tabCheck"){
+		checkIfOld()
+	}
+})
+
+chrome.runtime.onInstalled.addListener(init)
+chrome.runtime.onStartup.addListener(init)
+chrome.runtime.onSuspend.addListener(cleanUp)
+chrome.runtime.onSuspendCanceled.addListener(init)
+chrome.runtime.onConnect.addListener(function(){
+	console.log("Connected...?")
+})
 
